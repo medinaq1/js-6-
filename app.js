fbb16e6036a73d77d4c32d6d2ce06ba4ee325e7f
medinaq1 <amangulovamedina006@gmail.com>
2024-10-12 01:37:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+//if else swich cese 
+//цикл
+// while ,do while, for .
+
+// while (  условия ){
+//     код 
+// }
+
+// let num = 0 
+
+// while (num<=10) {
+//     console.log(num);
+//     num ++
+
+    
+// // }
+// do {
+//     //код
+// }while ( условия )
+
+// var  num = 0 
+// do{
+//     console.log(num)
+//     num++;
+//     }    while ( num <= 30)
+    
+// for (  начало условие шаг ){
+// }
+
+// for ( let num = 0; num <=10; num ++){
+//     console.log(num);
+    
+// }
+    
+
+    // for  ( let i = 20; 0 < 1; i --){
+    //     console.log(i);
+        
+    // }
+    // for (let i = 2; i <= 50; i++) {
+    //     if (i % 2 == 0) {
+    //      console.log(i);
+         
+    //     }
+    //   }
+                                                    
+
+
+
+for ( let i =0; i <= 500; i ++){
+    if (i % 3 === 0 )
+         {
+                console.log ( i,`cola`) ;
+    }else if ( i % 7 === 0 )
+        {
+                console.log( i,`cola`) ;    
+    }if ( i % 15 === 0 )
+        {
+                console.log(i, `coco cola`) ;
+    
+    }
+    else{
+        console.log ( i ) ;
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
